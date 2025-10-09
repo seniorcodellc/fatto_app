10fa7b3163a34c820b5a2ba298004e7b43376cfb
ahmed-sobhy-coder <67193348+ahmed-sobhy-coder@users.noreply.github.com>
2025-10-09 10:21:11 +0200

    project setup

---
+++ android/app/src/main/kotlin/com/fatto/seniorcode/fattto/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.fatto.seniorcode.fattto
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+class AppColors{
+  static const Color primaryColor = Color(0xFFBD6533);
+
+}
