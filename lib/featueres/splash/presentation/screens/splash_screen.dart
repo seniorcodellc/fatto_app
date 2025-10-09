4c4f8affa2d28528bae95af44c6c9dd84ba764d2
LailaRagab <lailaragab144@gmail.com>
2025-10-09 16:58:49 +0300

    Working on change app logo and splash screen.

---
+++ lib/featueres/splash/presentation/screens/splash_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(child: Image.asset("assets/images/splash.png")),
+      ),
+    );
+  }
+}
