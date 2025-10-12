9969c92772e5504bfe101584a9c4f6fb38bd9bac
LailaRagab <lailaragab144@gmail.com>
2025-10-12 13:46:19 +0300

    push

--- lib/featueres/splash/presentation/screens/splash_screen.dart
+++ lib/featueres/splash/presentation/screens/splash_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
-
+  static final route = "splash_screen";
   @override
   Widget build(BuildContext context) {
     return SafeArea(
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:fattto/featueres/splash/presentation/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 
+import 'featueres/home/presentation/screens/home_screen.dart';
+
 void main() {
   runApp(FattoApp());
 }
@@ -10,6 +12,14 @@ class FattoApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: SplashScreen());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+      initialRoute: 'splash_screen',
+      routes: {
+        'splash_screen': (context) => SplashScreen(),
+        'home_screen': (context) => HomeScreen(),
+      },
+    );
   }
 }
